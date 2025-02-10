dbbe453c57311879aa2b65de574430569908fd8c
DELIN-Alexis-2225027aa <114407197+DELIN-Alexis-2225027aa@users.noreply.github.com>
2025-02-10 11:16:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__)*
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/gougougaga')
+def testroute():
+    return 'Yeah! ça marche'
